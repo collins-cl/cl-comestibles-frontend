0761cbc2f4d7defe8cf09e4f89eb1644e0eb518c
Collins-cl <collinsolads@gmail.com>
2022-08-26 21:28:03 +0100

    testing offers

--- src/Components/OfferDummy.jsx
+++ src/Components/OfferDummy.jsx
@@ -4,28 +4,28 @@ const OfferDummys = [
     name: "KETONE-DIET",
     description:
       "Strictly made ketone diet with fresh vegetables an eggs",
-    image: "/Images/offer-image-1.jpg",
+    image: "/Images/food4.png",
   },
   {
     id: 2,
     name: "ITALIAN PASTA",
     description:
       "Taste the sensational italian pasta made by our chef",
-    image: "/Images/offer-image-3.jpg",
+    image: "/Images/food5.png",
   },
   {
     id: 3,
     name: "PANCAKE DROP",
     description:
       "Pancake dough complimented with geniune honey jelly",
-    image: "/Images/offer-image-2.jpg",
+    image: "/Images/food2.png",
   },
   {
     id: 4,
     name: "AFRICAN JELLOF",
     description:
       "Arial view of jellof rice with stew and beef",
-    image: "/Images/offer-image-4.jpg",
+    image: "/Images/food1.png",
   },
 ];
 
--- src/Components/Offers/Offer/Offer.jsx
+++ src/Components/Offers/Offer/Offer.jsx
@@ -2,19 +2,21 @@ import React from "react";
 import "../Offer/Offer.css";
 import { Link } from "react-router-dom";
 
-function Offer({offer}) {
+function Offer({ offer }) {
   return (
     // boiler plate for each offer products that would be mapped in the offers component
 
     <div className="offer">
-      <div className="offer-cont">
-        <div className="offer-image">
-          <img src={offer.image} alt="" />
-        </div>
-        <div className="offername">{offer.name}</div>
-        <div className="offer-desc">{offer.description}</div>
-        <div className="shop-now">
-          <Link to="/shop">SHOP NOW</Link>
+      <div className="wrapper">
+        <div className="offer-cont">
+          <div className="offer-image">
+            <img src={offer.image} alt="" />
+          </div>
+          <div className="offername">{offer.name}</div>
+          <div className="offer-desc">{offer.description}</div>
+          <div className="shop-now">
+            <Link to="/shop">SHOP NOW</Link>
+          </div>
         </div>
       </div>
     </div>

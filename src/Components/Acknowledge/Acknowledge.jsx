5bb1551633aada127d6f8d4fa0ba04077660b3e7
Collins-cl <collinsolads@gmail.com>
2022-08-26 02:43:36 +0100

    discovered grid

--- src/Components/Acknowledge/Acknowledge.jsx
+++ src/Components/Acknowledge/Acknowledge.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import "../Acknowledge/acknowledge.css";
+import AckCss from "../Acknowledge/Acknowledge.module.css";
 import pic6 from "../Assets/pic6.jpg";
 
 function Acknowledge() {
   return (
-    <div className="ark">
-      <div className="ark-container">
-        <div className="container-1">
-          <div className="head">No. 1 Ranked Food Store</div>
-          <div className="details">
+    <div className={AckCss.ark}>
+      <div className={AckCss.arkcontainer}>
+        <div className={AckCss.container1}>
+          <div className={AckCss.head}>No. 1 Ranked Food Store</div>
+          <div className={AckCss.details}>
             Since we launched in 2019 we’ve been on a mission to support the
             communities where we live, work and play. We’ve set out to offer
             better products, leave our communities better than before, and be
@@ -16,7 +16,7 @@ function Acknowledge() {
             be B Corp certified, for a better way to feel better.
           </div>
         </div>
-        <div className="container-2">
+        <div className={AckCss.container2}>
           <img src={pic6} alt="" />
         </div>
       </div>
--- src/Components/Picks/Picks.jsx
+++ src/Components/Picks/Picks.jsx
@@ -1,37 +1,35 @@
-import React from 'react'
-import "../Picks/Picks.css"
-import image1 from '../Assets/image1.jpg'
-import image2 from '../Assets/image2.jpg'
-import image3 from '../Assets/image3.jpg'
-import image4 from '../Assets/image4.jpg'
-import image5 from '../Assets/image9.jpg'
+import React from "react";
+import PicksCss from "../Picks/Picks.module.css";
+import image1 from "../Assets/image1.jpg";
+import image2 from "../Assets/image2.jpg";
+import image3 from "../Assets/image3.jpg";
+import image4 from "../Assets/image4.jpg";
+import image5 from "../Assets/image9.jpg";
 
 function Picks() {
   return (
-    <div className='picks'>
-        <div className="pickscontainer">
-            <div className="picks-1">
-                <img src={image1} alt="" />
-            </div>
-            <div className="picks-1">
-                <img src={image2} alt="" />
-            </div>
-            <div className="picks-1">
-                <img src={image3} alt="" />
-            </div>
-            <div className="picks-1">
-                <img src={image4} alt="" />
-            </div>
-            <div className="picks-1">
-                <img src={image5} alt="" />
-            </div>
+    <div className={PicksCss.picks}>
+      <div className={PicksCss.pickscontainer}>
+        <div className={PicksCss.picks1}>
+          <img src={image1} alt="" />
         </div>
+        <div className={PicksCss.picks1}>
+          <img src={image2} alt="" />
+        </div>
+        <div className={PicksCss.picks1}>
+          <img src={image3} alt="" />
+        </div>
+        <div className={PicksCss.picks1}>
+          <img src={image4} alt="" />
+        </div>
+        <div className={PicksCss.picks1}>
+          <img src={image5} alt="" />
+        </div>
+      </div>
 
-        <div className="picks-text">
-            OUR PICKS FOR YOU
-        </div>
+      <div className={PicksCss.pickstext}>OUR PICKS FOR YOU</div>
     </div>
-  )
+  );
 }
 
-export default Picks
+export default Picks;
